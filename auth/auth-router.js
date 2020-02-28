d25b5ac48f8338b5d7fd0ff0c6661d48dfc1015b
Sandra Coburn <sandra_coburn@hotmail.com>
2020-02-28 09:30:46 -0800

    Added dependencies, finished auth router for users, added authentication middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,53 @@
-const router = require('express').Router();
+require("dotenv").config();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+const Users = require("../users/users-model");
+const { jwtSecret } = require("../config/secrets");
+
+router.post("/register", (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error with the server" });
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
+      } else {
+        res.status(401).json({ message: "invalid credentials" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error at login" });
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1hr"
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
