986b3da67278c9c252dadc6e0e93cc7960f679ad
Sandra Coburn <sandra_coburn@hotmail.com>
2020-02-28 10:56:23 -0800

    Created 8 passing tests

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const request = require("supertest");
 const server = require("../api/server");
+const app = require("../auth/auth-router");
+const jokes = require("../jokes/jokes-router");
 
 describe("Auth router", function() {
   it("should run the tests", function() {
     expect(true).toBe(true);
+  });
+  it("should return a JSON object from the Auth router", async () => {
+    const response = await request(server).get("/");
+
+    expect(response.type).toEqual("application/json");
   });
 });
 
@@ -24,15 +31,37 @@ describe("index route", () => {
 
     expect(response.body).toEqual(expectedBody);
   });
-
-  it("should return a JSON object from the index route", async () => {
-    const response = await request(server).get("/");
-
-    expect(response.type).toEqual("application/json");
-  });
 });
 describe("Server", function() {
   it("Should use the testing environment", function() {
     expect(process.env.DB_ENV).toBe("development");
+  });
+});
+describe("Post", function() {
+  it("Should return an OK status to the post route", function() {
+    const expectedStatusCode = 201;
+    request(app)
+      .post("/register")
+      .send({ name: "test", password: "pass" });
+
+    expect(expectedStatusCode);
+  });
+});
+
+describe("Get users endpoint", function() {
+  it("Should return an ok status to get route", function() {
+    const expectedStatusCode = 200;
+    request(app).post("/users");
+
+    expect(expectedStatusCode);
+  });
+});
+
+describe("Get jokes endpoint", function() {
+  it("Should return an ok status to get route", function() {
+    const expectedStatusCode = 200;
+    request(jokes).post("/jokes");
+
+    expect(expectedStatusCode);
   });
 });
